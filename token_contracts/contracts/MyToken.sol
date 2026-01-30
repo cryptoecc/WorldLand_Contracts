59d61596bb28bed5a5a82db20aebd8c8d9442a76
nam2ee <nyj5404@yonsei.ac.kr>
2026-01-30 17:49:34 +0900

    token_contracts

---
+++ token_contracts/contracts/MyToken.sol
@@ -0,0 +1,14 @@
+
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WorldLandNativeToken is ERC20 {
+    constructor(address foundation_wallet) 
+        ERC20("WorldLandNativeToken", "WL") 
+    {
+        _mint(foundation_wallet, 1_000_000_000 * 10 ** decimals());
+    }
+}
